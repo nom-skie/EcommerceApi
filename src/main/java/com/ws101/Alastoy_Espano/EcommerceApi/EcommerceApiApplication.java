5396866d756e636060e7d9f8ae1903ad01940b9a
nom-skie <jayson2k03@gmail.com>
2026-04-22 01:10:31 +0800

    feat: initialized project structure and standard packages

---
+++ src/main/java/com/ws101/Alastoy_Espano/EcommerceApi/EcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ws101.Alastoy_Espano.EcommerceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiApplication.class, args);
+	}
+
+}
