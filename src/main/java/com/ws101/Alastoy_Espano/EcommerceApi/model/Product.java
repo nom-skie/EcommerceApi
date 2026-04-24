c071850cd1c01ac5bd69a8bce806cd19a33f439e
Mila Jane Espano <espanomilajane@gmail.com>
2026-04-24 23:24:10 +0800

    feat: implemented product model with constructors and accessors

---
+++ src/main/java/com/ws101/Alastoy_Espano/EcommerceApi/model/Product.java
@@ -0,0 +1,20 @@
+package com.ws101.Alastoy_Espano.EcommerceApi.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private int stockQuantity;
+    private String imageUrl;
+}
